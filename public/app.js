bb91c59f93bbe183fa2694af120cd3c19c123e00
Harjit <harjitk89@hotmail.co.uk>
2018-08-05 22:09:30 +0100

    some styling added

--- public/app.js
+++ public/app.js
@@ -1,121 +1,114 @@
-const app = function(){
-  const url = 'https://newsapi.org/v2/top-headlines?country=gb&category=entertainment&apiKey=c94ead78503f432ab71fee4684f11855';
+var app = function() {
+    const url =
+        "https://newsapi.org/v2/top-headlines?country=gb&category=entertainment&apiKey=c94ead78503f432ab71fee4684f11855";
 
-  makeRequest(url, requestComplete);
+    makeRequest(url, requestComplete);
+};
 
-}
+const makeRequest = function(url, callback) {
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.addEventListener("load", callback);
+    request.send();
+};
 
-const makeRequest = function(url, callback){
-  const request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.addEventListener('load', callback);
-  request.send();
-}
+// populateList  strts here
+const populateList = function(articles) {
+    const div = document.getElementById("article-list");
 
-const populateList = function(articles){
+    var unique_array = [];
 
-  const div = document.getElementById('article-list');
+    articles.forEach(function(article, index) {
 
-  const selectList = document.createElement("select");
-  selectList.setAttribute("id", "mySelect");
-
-
-  articles.forEach(function(article, index){
-
-    const articleImage = document.createElement('img');
-    if (articleImage.src === null){
-      articleImage.src = './ENewslogo.jpeg'
-  } else {
-    articleImage.src = article.urlToImage
-  }
+        console.log(unique_array);
 
 
-const articleUrl = document.createElement('a')
-articleUrl.href =  article.url;
-articleUrl.textContent = '     see more';
-// ^use a for a href link
+//this pushes the article name to the unique array if it is not already there
+//eliminating duplicates
+        if (unique_array.indexOf(article.source.name) == -1) {
+            unique_array.push(article.source.name);
+        }
 
-const articleTitle = document.createElement('h2');
-articleTitle.innerText = article.title;
+        const articleImage = document.createElement("img");
+        if (articleImage.src === null) {
+            articleImage.src = "./ENewslogo.jpeg";
+        } else {
+            articleImage.src = article.urlToImage;
+        }
 
+        const articleUrl = document.createElement("a");
+        articleUrl.href = article.url;
+        articleUrl.textContent = "see more";
+        // ^use a for a href link
 
-const articleName = document.createElement('p');
-articleName.innerText = article.source.name;
+        const articleTitle = document.createElement("h2");
+        articleTitle.innerText = article.title;
 
+        const articleName = document.createElement("p");
+        articleName.innerText = article.source.name;
 
-const articleAuthor = document.createElement('p');
-articleAuthor.innerText = article.author;
+        const articleAuthor = document.createElement("p");
+        articleAuthor.innerText = article.author;
 
+        const articleDescription = document.createElement("p");
+        const description = _.unescape(article.description);
+        // ^use lodash to deal with unnecessary html
+        articleDescription.innerText = description;
 
-const articleDescription = document.createElement('p');
-const description = _.unescape(article.description);
+        div.appendChild(articleImage);
+        div.appendChild(articleTitle);
+        div.appendChild(articleName);
+        div.appendChild(articleAuthor);
+        div.appendChild(articleDescription);
+        div.appendChild(articleUrl);
+    });
 
-// ^use lodash to deal with unnecessary html
+    var selectList = document.createElement("select");
+    selectList.setAttribute("id", "mySelect");
+    myDiv.appendChild(selectList);
+    for (var i = 0; i < unique_array.length; i++) {
+        var option = document.createElement("option");
+        option.setAttribute("value", unique_array[i]);
+        option.text = unique_array[i];
+        selectList.appendChild(option);
+    }
 
-articleDescription.innerText = description;
+    selectList.addEventListener("change", populateSelect);
+};
 
-// const articleDate = document.createElement('p');
-// articleDate.innerText = article.publishedAt;
-
-myDiv.appendChild(selectList);
-const option = document.createElement("option");
-option.setAttribute("value", article.source.id);
-option.text = article.source.id;
-selectList.appendChild(option);
-
-
-
-div.appendChild(articleImage);
-div.appendChild(articleUrl);
-div.appendChild(articleTitle);
-div.appendChild(articleName);
-div.appendChild(articleAuthor);
-div.appendChild(articleDescription);
-// div.appendChild(articleDate);
-})
-
-}
-
-//*** Populate DropDown in order to select articles based on
+// *** Populate DropDown in order to select articles based on
 // 'name'property from API ***
 
+const populateSelect = function() {
 
-
-// function sayHello(){
-//   alert('I am working ')
-//   const articleSelect = document.getElementById('article-select');
-//   // alert(object.toSource(articleSelect));
-// }
-
-const populateSelector = function(articles){
-  const articleSelect = document.getElementById('article-select');
-
-  for(const i = 0; i < articles.length; i++){
-    const article = articles[i];
-    const option = document.createElement('option');
-    option.innerText = article.name;
-    option.value = i;
-    articleSelect.appendChild(option)
-  }
-  articleSelect.addEventListener('change', function(){
-    const selectedName = articles[this.value];
-    const articleName = document.getElementById('article-list');
-    articleName.innerText = selectedName.source.name;
-
-  })
-}
-// //
+  var e = document.getElementById("mySelect");
+  var opVal = e.options[e.selectedIndex].value;
+  console.log(opVal);
 
 
 
-const requestComplete = function(){
-  if(this.status != 200) return;
-  const jsonString = this.responseText;
-  const articles = JSON.parse(jsonString);
-  populateList(articles.articles);
-  // ^because articles array is nested within an object
-}
+  //
+  // const newArray = [articleImage, articleUrl, articleTitle, articleName, articleAuthor, articleDescription];
+
+  // const new = fruits.indexOf("Apple");
+  // const result = articleName.filter(articleName => articleName.length > 6);
+  //
+  // console.log(result);
 
 
 
-window.addEventListener('load', app);
+
+};
+
+
+
+
+const requestComplete = function() {
+    if (this.status != 200) return;
+    const jsonString = this.responseText;
+    const articles = JSON.parse(jsonString);
+    populateList(articles.articles);
+    // ^because articles array is nested within an object
+};
+
+window.addEventListener("load", app);
---
+++ public/original_app.js
@@ -0,0 +1,128 @@
+const app = function(){
+  const url = 'https://newsapi.org/v2/top-headlines?country=gb&category=entertainment&apiKey=c94ead78503f432ab71fee4684f11855';
+
+  makeRequest(url, requestComplete);
+
+}
+
+const makeRequest = function(url, callback){
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+}
+
+const populateList = function(articles){
+
+  const div = document.getElementById('article-list');
+
+  const selectList = document.createElement("select");
+  selectList.setAttribute("id", "mySelect");
+
+//   const newArticles = Articles.filter(function(elem, pos) {
+//   return Articles.indexOf(elem) == pos;
+// });
+
+
+  articles.forEach(function(article, index){
+
+    const articleImage = document.createElement('img');
+    if (articleImage.src === null){
+      articleImage.src = './ENewslogo.jpeg'
+  } else {
+    articleImage.src = article.urlToImage
+  }
+
+
+const articleUrl = document.createElement('a')
+articleUrl.href =  article.url;
+articleUrl.textContent = '     see more';
+// ^use a for a href link
+
+const articleTitle = document.createElement('h2');
+articleTitle.innerText = article.title;
+
+
+const articleName = document.createElement('p');
+articleName.innerText = article.source.name;
+
+
+const articleAuthor = document.createElement('p');
+
+
+
+articleAuthor.innerText = article.author;
+
+
+const articleDescription = document.createElement('p');
+const description = _.unescape(article.description);
+
+// ^use lodash to deal with unnecessary html
+
+articleDescription.innerText = description;
+
+// const articleDate = document.createElement('p');
+// articleDate.innerText = article.publishedAt;
+
+myDiv.appendChild(selectList);
+const option = document.createElement("option");
+option.setAttribute("value", article.source.id);
+option.text = article.source.id;
+selectList.appendChild(option);
+
+
+
+div.appendChild(articleImage);
+div.appendChild(articleUrl);
+div.appendChild(articleTitle);
+div.appendChild(articleName);
+div.appendChild(articleAuthor);
+div.appendChild(articleDescription);
+// div.appendChild(articleDate);
+})
+
+}
+
+//*** Populate DropDown in order to select articles based on
+// 'name'property from API ***
+
+
+// 
+// function sayHello(){
+//   alert('I am working ')
+//   const articleSelect = document.getElementById('article-select');
+//   // alert(object.toSource(articleSelect));
+// }
+
+const populateSelector = function(articles){
+  const articleSelect = document.getElementById('article-select');
+
+  for(const i = 0; i < articles.length; i++){
+    const article = articles[i];
+    const option = document.createElement('option');
+    option.innerText = article.name;
+    option.value = i;
+    articleSelect.appendChild(option)
+  }
+  articleSelect.addEventListener('change', function(){
+    const selectedName = articles[this.value];
+    const articleName = document.getElementById('article-list');
+    articleName.innerText = selectedName.source.name;
+
+  })
+}
+// //
+
+
+
+const requestComplete = function(){
+  if(this.status != 200) return;
+  const jsonString = this.responseText;
+  const articles = JSON.parse(jsonString);
+  populateList(articles.articles);
+  // ^because articles array is nested within an object
+}
+
+
+
+window.addEventListener('load', app);
